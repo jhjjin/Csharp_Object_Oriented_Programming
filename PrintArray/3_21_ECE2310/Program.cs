59e0aee7dc11a7ecbf9f6e26600ca56711ff882d
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2024-11-17 15:50:26 -0800

    Add files via upload

---
+++ PrintArray/3_21_ECE2310/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_21_ECE2310
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] k = new int[10];
+            for (int j =0; j < 10; j++)
+            {
+                k[j] = j + 1;
+
+            }
+            PrintArray(k);
+            Console.Read();
+        }
+        static void PrintArray(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+                Console.WriteLine(" Index : {0}\t Element : {1}", i, x[i]);
+            
+        }
+    }
+}
