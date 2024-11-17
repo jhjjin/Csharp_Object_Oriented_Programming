b5a406a7a94dda9a0b2912a825893d39624abebe
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2024-11-17 15:51:39 -0800

    Add files via upload

---
+++ Window_Score_Generator_Curve/3_21_ECE2310_window/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace _3_21_ECE2310_window
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(412, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(330, 161);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(85, 495);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(346, 479);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(533, 495);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(346, 479);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 442);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Before Curved";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(619, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "After Curved";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 1119);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Window_Score_Generator_Curve/3_21_ECE2310_window/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_21_ECE2310_window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numOfStudents = 26;
+            int[] score = GenerateRandom(numOfStudents, 60, 100);
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Student ID#\tScore");
+            for (int i = 0; i < numOfStudents; i++)
+                listBox1.Items.Add(i.ToString() + "\t\t" + score[i].ToString());
+            listBox1.Items.Add("The average score is" + Ave(score).ToString());
+            double r = 85.0 / Ave(score);
+            if (r < 1) // The average of class is over 85
+                MessageBox.Show("The class is doing good");
+            else
+                MessageBox.Show("Curbe the scores with the ratio of " + r.ToString());
+
+            //double[] newScores = Curved(score, r);
+            Curved(score, r); 
+            for (int i = 0; i < numOfStudents; i++)
+                listBox2.Items.Add(i.ToString() + "\t\t" + score[i].ToString());
+        }
+        /*double[] Curved(int[] x, double ratio)
+        {
+            double[] y = new double[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                y[i] = x[i] * ratio;
+                y[i] = Math.Round(y[i], 0);
+                if (y[i] > 100) y[i] = 100;
+            }
+            return y;
+        }*/
+        void Curved(int[] x, double ratio)
+        {
+            double[] y = new double[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                y[i] = x[i] * ratio;
+                y[i] = Math.Round(y[i], 0);
+                if (y[i] > 100) y[i] = 100;
+                x[i] = Convert.ToInt32(y[i]);
+            }
+           
+        }
+        int[] GenerateRandom(int size, int low, int up)
+        {
+            int[] x = new int[size];
+            Random x0 = new Random();
+            for (int i = 0; i < size; i++)
+                x[i] = x0.Next(low, up + 1);
+            return x;
+        }
+        int Ave(int[] x)
+        {
+            // ideal  =85 ave 70 ratio = ia/ ave -> x[i] *= i + ratio / cap 100
+            int ave = 0;
+            for (int i = 0; i < x.Length; i++)
+                ave += x[i];
+            return ave/x.Length;
+            
+            
+        }
+        
+    }
+}
