0f241fc3f84e54c4a5bf979b3fe4a9833b95e15e
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2024-11-17 16:04:09 -0800

    Add files via upload

---
+++ AreaOfPentagon Window/Exam1Window/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Exam1Window
+{
+    partial class Calculation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x1 = new System.Windows.Forms.Label();
+            this.y1 = new System.Windows.Forms.Label();
+            this.x2 = new System.Windows.Forms.Label();
+            this.y2 = new System.Windows.Forms.Label();
+            this.x3 = new System.Windows.Forms.Label();
+            this.y3 = new System.Windows.Forms.Label();
+            this.x4 = new System.Windows.Forms.Label();
+            this.y4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtY1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.txtY2 = new System.Windows.Forms.TextBox();
+            this.txtX3 = new System.Windows.Forms.TextBox();
+            this.txtY3 = new System.Windows.Forms.TextBox();
+            this.txtX4 = new System.Windows.Forms.TextBox();
+            this.txtY4 = new System.Windows.Forms.TextBox();
+            this.areaOfpentagon = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.x5 = new System.Windows.Forms.Label();
+            this.txtX5 = new System.Windows.Forms.TextBox();
+            this.txtY5 = new System.Windows.Forms.TextBox();
+            this.y5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // x1
+            // 
+            this.x1.AutoSize = true;
+            this.x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x1.Location = new System.Drawing.Point(113, 122);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(43, 29);
+            this.x1.TabIndex = 0;
+            this.x1.Text = "x1:";
+            // 
+            // y1
+            // 
+            this.y1.AutoSize = true;
+            this.y1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y1.Location = new System.Drawing.Point(431, 122);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(43, 29);
+            this.y1.TabIndex = 1;
+            this.y1.Text = "y1:";
+            this.y1.Click += new System.EventHandler(this.y1_Click);
+            // 
+            // x2
+            // 
+            this.x2.AutoSize = true;
+            this.x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x2.Location = new System.Drawing.Point(113, 225);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(43, 29);
+            this.x2.TabIndex = 2;
+            this.x2.Text = "x2:";
+            // 
+            // y2
+            // 
+            this.y2.AutoSize = true;
+            this.y2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y2.Location = new System.Drawing.Point(431, 225);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(43, 29);
+            this.y2.TabIndex = 3;
+            this.y2.Text = "y2:";
+            // 
+            // x3
+            // 
+            this.x3.AutoSize = true;
+            this.x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x3.Location = new System.Drawing.Point(113, 331);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(43, 29);
+            this.x3.TabIndex = 4;
+            this.x3.Text = "x3:";
+            // 
+            // y3
+            // 
+            this.y3.AutoSize = true;
+            this.y3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y3.Location = new System.Drawing.Point(431, 331);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(43, 29);
+            this.y3.TabIndex = 5;
+            this.y3.Text = "y3:";
+            // 
+            // x4
+            // 
+            this.x4.AutoSize = true;
+            this.x4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x4.Location = new System.Drawing.Point(113, 460);
+            this.x4.Name = "x4";
+            this.x4.Size = new System.Drawing.Size(43, 29);
+            this.x4.TabIndex = 6;
+            this.x4.Text = "x4:";
+            // 
+            // y4
+            // 
+            this.y4.AutoSize = true;
+            this.y4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y4.Location = new System.Drawing.Point(431, 449);
+            this.y4.Name = "y4";
+            this.y4.Size = new System.Drawing.Size(43, 29);
+            this.y4.TabIndex = 7;
+            this.y4.Text = "y4:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(228, 607);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(219, 64);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(521, 870);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(219, 64);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(774, 870);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(219, 64);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(228, 122);
+            this.txtX1.Multiline = true;
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(150, 44);
+            this.txtX1.TabIndex = 11;
+            // 
+            // txtY1
+            // 
+            this.txtY1.Location = new System.Drawing.Point(521, 107);
+            this.txtY1.Multiline = true;
+            this.txtY1.Name = "txtY1";
+            this.txtY1.Size = new System.Drawing.Size(150, 44);
+            this.txtY1.TabIndex = 12;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(228, 210);
+            this.txtX2.Multiline = true;
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(150, 44);
+            this.txtX2.TabIndex = 13;
+            // 
+            // txtY2
+            // 
+            this.txtY2.Location = new System.Drawing.Point(521, 210);
+            this.txtY2.Multiline = true;
+            this.txtY2.Name = "txtY2";
+            this.txtY2.Size = new System.Drawing.Size(150, 44);
+            this.txtY2.TabIndex = 14;
+            // 
+            // txtX3
+            // 
+            this.txtX3.Location = new System.Drawing.Point(228, 316);
+            this.txtX3.Multiline = true;
+            this.txtX3.Name = "txtX3";
+            this.txtX3.Size = new System.Drawing.Size(150, 44);
+            this.txtX3.TabIndex = 15;
+            // 
+            // txtY3
+            // 
+            this.txtY3.Location = new System.Drawing.Point(521, 331);
+            this.txtY3.Multiline = true;
+            this.txtY3.Name = "txtY3";
+            this.txtY3.Size = new System.Drawing.Size(150, 44);
+            this.txtY3.TabIndex = 16;
+            // 
+            // txtX4
+            // 
+            this.txtX4.Location = new System.Drawing.Point(228, 449);
+            this.txtX4.Multiline = true;
+            this.txtX4.Name = "txtX4";
+            this.txtX4.Size = new System.Drawing.Size(150, 44);
+            this.txtX4.TabIndex = 17;
+            // 
+            // txtY4
+            // 
+            this.txtY4.Location = new System.Drawing.Point(521, 460);
+            this.txtY4.Multiline = true;
+            this.txtY4.Name = "txtY4";
+            this.txtY4.Size = new System.Drawing.Size(150, 44);
+            this.txtY4.TabIndex = 18;
+            // 
+            // areaOfpentagon
+            // 
+            this.areaOfpentagon.AutoSize = true;
+            this.areaOfpentagon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.areaOfpentagon.Location = new System.Drawing.Point(169, 727);
+            this.areaOfpentagon.Name = "areaOfpentagon";
+            this.areaOfpentagon.Size = new System.Drawing.Size(209, 29);
+            this.areaOfpentagon.TabIndex = 19;
+            this.areaOfpentagon.Text = "Area Of Pentagon:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(485, 712);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(306, 44);
+            this.txtOutput.TabIndex = 20;
+            // 
+            // x5
+            // 
+            this.x5.AutoSize = true;
+            this.x5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x5.Location = new System.Drawing.Point(113, 535);
+            this.x5.Name = "x5";
+            this.x5.Size = new System.Drawing.Size(43, 29);
+            this.x5.TabIndex = 21;
+            this.x5.Text = "x5:";
+            // 
+            // txtX5
+            // 
+            this.txtX5.Location = new System.Drawing.Point(228, 535);
+            this.txtX5.Multiline = true;
+            this.txtX5.Name = "txtX5";
+            this.txtX5.Size = new System.Drawing.Size(150, 44);
+            this.txtX5.TabIndex = 22;
+            // 
+            // txtY5
+            // 
+            this.txtY5.Location = new System.Drawing.Point(521, 535);
+            this.txtY5.Multiline = true;
+            this.txtY5.Name = "txtY5";
+            this.txtY5.Size = new System.Drawing.Size(150, 44);
+            this.txtY5.TabIndex = 23;
+            // 
+            // y5
+            // 
+            this.y5.AutoSize = true;
+            this.y5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y5.Location = new System.Drawing.Point(431, 535);
+            this.y5.Name = "y5";
+            this.y5.Size = new System.Drawing.Size(37, 29);
+            this.y5.TabIndex = 24;
+            this.y5.Text = "y5";
+            // 
+            // Calculation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 964);
+            this.Controls.Add(this.y5);
+            this.Controls.Add(this.txtY5);
+            this.Controls.Add(this.txtX5);
+            this.Controls.Add(this.x5);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.areaOfpentagon);
+            this.Controls.Add(this.txtY4);
+            this.Controls.Add(this.txtX4);
+            this.Controls.Add(this.txtY3);
+            this.Controls.Add(this.txtX3);
+            this.Controls.Add(this.txtY2);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtY1);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.y4);
+            this.Controls.Add(this.x4);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Name = "Calculation";
+            this.Text = "Calculation Area";
+            this.Load += new System.EventHandler(this.Calculation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label x1;
+        private System.Windows.Forms.Label y1;
+        private System.Windows.Forms.Label x2;
+        private System.Windows.Forms.Label y2;
+        private System.Windows.Forms.Label x3;
+        private System.Windows.Forms.Label y3;
+        private System.Windows.Forms.Label x4;
+        private System.Windows.Forms.Label y4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtY1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.TextBox txtY2;
+        private System.Windows.Forms.TextBox txtX3;
+        private System.Windows.Forms.TextBox txtY3;
+        private System.Windows.Forms.TextBox txtX4;
+        private System.Windows.Forms.TextBox txtY4;
+        private System.Windows.Forms.Label areaOfpentagon;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label x5;
+        private System.Windows.Forms.TextBox txtX5;
+        private System.Windows.Forms.TextBox txtY5;
+        private System.Windows.Forms.Label y5;
+    }
+}
+
---
+++ AreaOfPentagon Window/Exam1Window/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam1Window
+{
+    public partial class Calculation : Form
+    {
+        public Calculation()
+        {
+            InitializeComponent();
+        }
+
+        private void y1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtX1.Clear();
+            txtX2.Clear();
+            txtX3.Clear();
+            txtX4.Clear();
+            txtY1.Clear();
+            txtY2.Clear();
+            txtY3.Clear();
+            txtY4.Clear();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double x1,x2,x3,x4, x5,y1,y2,y3,y4,y5;
+            x1 = Double.Parse(txtX1.Text);
+            x2 = Double.Parse(txtX2.Text);
+            x3 = Double.Parse(txtX3.Text);
+            x4 = Double.Parse(txtX4.Text);
+            x5 = Double.Parse(txtX5.Text);
+            y1 = Double.Parse(txtY1.Text);
+            y2 = Double.Parse(txtY2.Text);
+            y3 = Double.Parse(txtY3.Text);
+            y4 = Double.Parse(txtY4.Text);
+            y5 = Double.Parse(txtY5.Text);
+
+            bool valid1 = double.TryParse(txtX1.Text, out x1);
+            if (!valid1)
+                MessageBox.Show("Invalid input for x1.");
+            bool valid2 = double.TryParse(txtX2.Text, out x2);
+            if (!valid2)
+                MessageBox.Show("Invalid input for x2.");
+            bool valid3 = double.TryParse(txtX3.Text, out x3);
+            if (!valid3)
+                MessageBox.Show("Invalid input for x3.");
+            bool valid4 = double.TryParse(txtX4.Text, out x4);
+            if (!valid4)
+                MessageBox.Show("Invalid input for x4.");
+            bool valid5 = double.TryParse(txtX5.Text, out x5);
+            if (!valid5)
+                MessageBox.Show("Invalid input for x5.");
+            bool valid6 = double.TryParse(txtY1.Text, out y1);
+            if (!valid6)
+                MessageBox.Show("Invalid input for y1.");
+            bool valid7 = double.TryParse(txtY2.Text, out y2);
+            if (!valid7)
+                MessageBox.Show("Invalid input for y2.");
+            bool valid8 = double.TryParse(txtY3.Text, out y3);
+            if (!valid8)
+                MessageBox.Show("Invalid input for y3.");
+            bool valid9 = double.TryParse(txtY4.Text, out y4);
+            if (!valid9)
+                MessageBox.Show("Invalid input for y4.");
+            bool valid10 = double.TryParse(txtY5.Text, out y5);
+            if (!valid10)
+                MessageBox.Show("Invalid input for y5.");
+
+            double a = Distance2(x1, x2, y1, y2);
+            double b = Distance2(x1, x3, y1, y3);
+            double c = Distance2(x3, x2, y3, y2);
+            double d = Distance2(x3, x5, y3, y5);
+            double f = Distance2(x5, x2, y5, y2);
+            double c2 = Distance2(x3, x2, y3, y2);
+            double g = Distance2(x2, x4, y2, y4);
+            double h = Distance2(x4, x5, y5, y4);
+            double i = Distance2(x5, x2, y5, y2);
+
+            double t1 = AreaOfTriangle(a,b,c);
+            double t2 = AreaOfTriangle(d, f, c2);
+            double t3 = AreaOfTriangle(g, h, i);
+            double Area = t1 + t2 + t3;
+            txtOutput.Text = Area.ToString("0.00");
+
+
+        }
+        public static double AreaOfTriangle(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt((s) * (s - a) * (s - b) * (s - c));
+           
+
+        }
+        public static double Distance2(double x1, double x2, double y1, double y2)
+        {
+
+            return Math.Sqrt((Math.Abs((x1 - x2) * (x1 - x2))) + (Math.Abs((y1 - y2) * (y1 - y2))));
+            
+        }
+
+        private void Calculation_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
