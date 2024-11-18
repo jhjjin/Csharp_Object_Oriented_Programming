21d93940431bf07a6aebb062d3abadb377a07f79
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2024-11-17 16:23:32 -0800

    Add files via upload

---
+++ Grad_Student_Info/HW11_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraduateStudent gradstudent = new GraduateStudent(100, "Hyukjin", "B.S", "Cal Poly Pomona");
+            UnderGraduate undergradStudent = new UnderGraduate(101, "Jeong", "Freshman");
+
+            Console.WriteLine("Graduate Stduent Info:");
+            gradstudent.Display();
+            Console.WriteLine("Undergraduate Student Info:");
+            undergradStudent.Display();
+
+            Console.Read();
+                
+        }
+    }
+}
---
+++ Grad_Student_Info/HW11_1/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11_1
+{
+    class Student
+    {
+        protected int studentID;
+        protected string name;
+
+        public Student(int ID, string nm)
+        {
+            studentID = ID;
+            name = nm;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("Student name is {0} and the ID is {1}", studentID, name);
+        }
+        ~Student() { }
+    }
+}
