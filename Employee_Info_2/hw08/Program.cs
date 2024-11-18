87e61db7a152ab4d358f49a9bd7785dc7fce4c21
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2024-11-17 16:18:24 -0800

    Add files via upload

---
+++ Employee_Info_2/hw08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Hyukjin", "Jeong", 101, 5000.0);
+            Employee employee2 = new Employee("James", "K", 102, 4000.0);
+
+            Console.Write("Employee 1: ");
+            employee1.DisplayInfo();
+            Console.Write("\nEmployee 2: ");
+            employee2.DisplayInfo();
+
+            
+            Console.Read();
+
+        }
+    }
+}
