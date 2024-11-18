e4b00850ccc32023d34ed76c4778441d8fcb6ceb
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2024-11-17 16:00:04 -0800

    Add files via upload

---
+++ Animal_List/ECE2310_4_30/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECE2310_4_30
+{
+    class Animal
+    {
+        protected int age;
+        protected string name;
+
+        public Animal()
+        {
+            age = 2;
+            name = "unknown";
+
+        }
+
+        public Animal(int a, string nm)
+        {
+            age = a;
+            name = nm;
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                    age = value;
+                else
+
+                    age = 0;
+
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public virtual void Move() //Override 
+        {
+            Console.WriteLine("{0} is moving quickly", name);
+        }
+        public override string ToString()
+        {
+            return "A " + age.ToString() + "-year-old animal by the name of " + name;
+        }
+        ~Animal() { }
+
+        
+    }
+}
