f454ced9940b9cb4d5f581e8df3cdaf7682455ab
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2024-11-17 16:24:58 -0800

    Add files via upload

---
+++ Bank_Deposit/HW11_2/Checking.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11_2
+{
+    class Checking:BankAccount
+    {
+        public Checking(string accountNum, string ownerNm, double bal) : base(accountNum, ownerNm, bal)
+        {
+
+        }
+        public override void Withdraw(double amount)
+        {
+            if(amount > balance)
+            {
+                Console.WriteLine("Invalid Number");
+                return;
+            }
+            balance -= amount;
+            Console.WriteLine("{0} amount withdrawn. New balance is {1}", amount, balance);
+        }
+        public override void Deposite(double amount)
+        {
+            balance += amount;
+            Console.WriteLine("{0} amount deposited. New balance is {1}", amount, balance);
+        }
+        public override void ToString()
+        {
+            Console.WriteLine("Account Number: {0} Ower Name: {1} Balance: {2}", accountNumber, ownerName, balance);
+        }
+        ~Checking() { }
+    }
+}
---
+++ Bank_Deposit/HW11_2/Saving.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11_2
+{
+    class Saving:BankAccount
+    {
+        public Saving(string accountNum, string owerName, double balance):base(accountNum, owerName, balance)
+        {
+
+        }
+        public override void Withdraw(double amount)
+        {
+            if(amount>balance)
+            {
+                Console.WriteLine("Invalid amount.");
+                return;
+            }
+            balance -= amount;
+            Console.WriteLine("{0} amount withdrawn. New balance: {1} ", amount, balance);
+        }
+        public override void Deposite(double amount)
+        {
+            balance += amount;
+            Console.WriteLine("{0} amount deposited. New balance: {1}", amount, balance);
+        }
+        public override void ToString()
+        {
+            Console.WriteLine("Account Number: {0} Ower Name: {1} Balance: {2}", accountNumber, ownerName, balance);
+        }
+        ~Saving() { }
+    }
+}
