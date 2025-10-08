ffd768cd93b2cbe6852018ac65e5050c2df2342c
Hyukjin Jeong <jeonghyukjin97@gmail.com>
2025-10-08 14:02:39 -0700

    Rename files/folders in BookData

---
+++ BookData/BookData/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookData
+{
+    class Book:ReadingMaterial 
+    {
+        private string author;
+        private Date publicationDate;
+
+        public Book(string titl,string Auth, Date publication)
+        {
+            Title = titl;
+            Author = Auth;
+            PublicationDate = publication;
+        }
+           
+        public string Author
+        {
+            get { return author; }
+            set { author = value;}
+        }
+        public Date PublicationDate
+        {
+            get { return publicationDate; }
+            set { publicationDate = value; }
+        }
+        public void Donate()
+        {
+            Console.WriteLine("Donate the book {0} and Date {1}", author, publicationDate);
+        }
+        public override void ToString()
+        {
+            Console.WriteLine("{0}, author is {1}, published on {2}", Title, Author,publicationDate);
+            
+        }
+        ~Book() { }
+    }
+}
---
+++ BookData/BookData/ReadingMaterial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookData
+{
+   class ReadingMaterial
+    {
+        private string title;
+        private double price;
+
+        public ReadingMaterial()
+        {
+            title = "Unknwon";
+            price = 0.0;
+        }
+        
+        public ReadingMaterial(string tt, double pc)
+        {
+            title = tt;
+            price = pc;
+
+        }
+        public string Title
+        {
+           get { return title; }
+            set { title = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public void Read()
+        {
+            Console.WriteLine("Read a book title: {0}", title);
+        }
+        public virtual void ToString()
+        {
+            Console.WriteLine();
+        }
+        ~ReadingMaterial() {}
+    }
+}
