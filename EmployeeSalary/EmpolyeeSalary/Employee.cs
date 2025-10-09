7ba0ba5dd32e09498407313f5e47e69fb11025c6
Hyukjin Jeong <jeonghyukjin97@gmail.com>
2025-10-09 14:22:20 -0700

    Renamed EmployeeSalary project files

---
+++ EmployeeSalary/EmpolyeeSalary/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpolyeeSalary
+{
+    internal class Employee
+    {
+        private int employeeNumber;
+        private string firstName;
+        private string lastName;
+        private double monthlySalary;
+
+        public Employee()
+        {
+            employeeNumber = 0;
+            firstName = "";
+            lastName = "";
+            monthlySalary = 0;
+        }
+        public Employee(string firstNm, string lastNm, int employeeNum, double monthlySal)
+        {
+            firstName = firstNm;
+            lastName = lastNm;
+            employeeNumber = employeeNum;
+            monthlySalary = monthlySal;
+        }
+        
+       
+        public double CalculateAnnualPay()
+        {
+            return monthlySalary * 12;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Employee Number: " + employeeNumber);
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+            Console.WriteLine("Monthly Salary: $ " + monthlySalary);
+            Console.WriteLine("Annual Pay: $ " + CalculateAnnualPay());
+        }
+    }
+}
